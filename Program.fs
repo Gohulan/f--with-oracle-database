336d625321b44b9b0e66b7abcfa257f444bb7295
Gohulan Somanathan <sgohul.24@gmail.com>
2023-02-06 15:28:06 +0530

    Add files via upload
    To develop an F# application that connects to an Oracle database in Visual Studio, you can choose any of the following frameworks:
    
    .NET Framework: This is a mature and well-established framework for building Windows desktop and server applications.
    
    .NET Core: This is a newer and cross-platform framework for building modern applications that can run on Windows, Linux, and macOS.
    
    Both frameworks support F# and the System.Data.OracleClient namespace, so you can use either one to develop your application. The choice between the two frameworks depends on your specific needs and requirements, such as the target platforms you want to run on, the features you need, and so on.
    
    If you're new to F# and Visual Studio, you can start by creating a new F# Console Application project in Visual Studio and following the example code I provided earlier.

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿#r "System.Data.OracleClient"
+
+open System
+open System.Data
+open System.Data.OracleClient
+
+// Connect to the database
+let connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=host)(PORT=port)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=service_name)));User Id=user_name;Password=password;"
+let connection = new OracleConnection(connectionString)
+connection.Open()
+
+// Create a command
+let command = new OracleCommand("SELECT * FROM your_table", connection)
+
+// Execute the command and get the reader
+let reader = command.ExecuteReader()
+
+// Loop through the results and print them
+while reader.Read() do
+    for i in 0 .. reader.FieldCount - 1 do
+        printfn "%s: %s" reader.GetName(i) reader.[GetValue(i)]
+    printfn ""
+
+// Close the reader and connection
+reader.Close()
+connection.Close()
